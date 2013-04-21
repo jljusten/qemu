6b1a78a0b893a09450c44132babec23c71051e60
Jordan Justen <jordan.l.justen@intel.com>
2013-04-21 16:49:44 -0700

    kvm: add kvm_readonly_mem_enabled
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- kvm-stub.c
+++ kvm-stub.c
@@ -22,6 +22,7 @@ bool kvm_async_interrupts_allowed;
 bool kvm_irqfds_allowed;
 bool kvm_msi_via_irqfd_allowed;
 bool kvm_gsi_routing_allowed;
+bool kvm_readonly_mem_allowed;
 
 int kvm_init_vcpu(CPUState *cpu)
 {
